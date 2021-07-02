d4a4e5165723501f33eec008041f90a151999557
Liekedv <84664965+Liekedv@users.noreply.github.com>
2021-07-02 08:29:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const makePizza = function () {
+  console.log ("Smeer tomatensaus op de pizzabodem");
+  console.log ("Verdeel de mozzarella over de pizzabodem");
+  console.log ("Verdeel de groentes over de pizzabodem");
+  console.log ("Strooi er iets olijfolie en kruiden over de pizza");
+  console.log ("Zet de pizza in de oven");
+  console.log ("Zet de oven aan op 200 graden");
+  console.log ("Haal de pizza naar 15 minuten uit de oven");
+};
+
+const makeSushi = function () {
+  console.log ("Kook de sushirijst 30 minuten in een pannetje met kokend water");
+  console.log ("Schil de komkommer");
+  console.log ("Snij de komkommer in de lengte door");
+  console.log ("Leg een vel nori op het bamboematje");
+  console.log ("Schep er 1 eetlepel rijst op");
+  console.log ("Spreid de rijst gelijkmatig uit over het norivel");
+  console.log ("Leg de reepjes komkommer in het midden");
+  console.log ("Rol de nori vel op");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
